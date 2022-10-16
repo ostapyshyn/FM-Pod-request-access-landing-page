ef636ff9fb0b03859a1f27e55278c89335a04a2f
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-10-16 16:51:56 +0300

    Add basic styles

---
+++ src/components/Pod.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Pod = () => {
+  return (
+    <main>
+      <article>Publish your podcasts everywhere.</article>
+      <p>
+        Upload your audio to Pod with a single click. We’ll then distribute your podcast to Spotify,
+        Apple Podcasts, Google Podcasts, Pocket Casts and more!
+      </p>
+      <input type="email" placeholder="Email address" />
+      <button>Request Access</button>
+    </main>
+  );
+};
+
+export default Pod;
