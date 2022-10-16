ae999681f7006e4fa5f0445620fb57e813445b3b
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-10-16 23:40:10 +0300

    Add styles to the text

--- src/components/Pod.jsx
+++ src/components/Pod.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
+import styles from '../assets/pod.module.scss';
 
 const Pod = () => {
   return (
     <main>
-      <article>Publish your podcasts everywhere.</article>
-      <p>
-        Upload your audio to Pod with a single click. We’ll then distribute your podcast to Spotify,
-        Apple Podcasts, Google Podcasts, Pocket Casts and more!
-      </p>
-      <input type="email" placeholder="Email address" />
-      <button>Request Access</button>
+      <article>
+        <h1>
+          Publish your podcasts <span>everywhere.</span>
+        </h1>
+
+        <p>
+          Upload your audio to Pod with a single click. We’ll then distribute your podcast to
+          Spotify, Apple Podcasts, Google Podcasts, Pocket Casts and more!
+        </p>
+
+        <div className={styles.email}>
+          <input
+            placeholder="Email address"
+            id="email"
+            autoComplete="off"
+            type="email"
+            // value={email}
+            // onChange={handleOnChange}
+          />
+          <button>Request Access</button>
+        </div>
+      </article>
     </main>
   );
 };
